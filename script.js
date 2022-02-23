f2ffe74ea04e75f0a1a8b610e665af8b9d604011
Ahsan Aman <76155759+ahsankhanan123@users.noreply.github.com>
2022-02-23 11:36:12 +0500

    dancingweb

---
+++ script.js
@@ -0,0 +1,22 @@
+var toggle = document.querySelector(".toggle");
+var nav = document.querySelector(".nav");
+
+toggle.addEventListener("click", function () {
+ toggle.classList.toggle("active");
+ nav.classList.toggle("active");
+});
+
+
+
+
+/* ============= Search ================ */
+
+var searchToggle = document.querySelector(".search-box");
+
+searchToggle.addEventListener("click", function () {
+     searchToggle.classList.toggle("show");
+
+    });
+/* ============= Search ================ */
+
+
